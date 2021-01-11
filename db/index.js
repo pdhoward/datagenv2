34b32dcf2ff0dfe8d58ada1150335849a0095e0f
pdhoward <patrick.howard@hotmail.com>
2021-01-11 14:01:23 -0500

    fix db conn'

--- db/index.js
+++ db/index.js
@@ -11,21 +11,15 @@ let proximityName = 'proximity'
 const machineurl = process.env.ATLAS_MACHINE_URI
 const proximityurl = process.env.ATLAS_PROXIMITY_URI
 
-// Atlas db with old collection of venues - start point for transforms
-let db = Mongo.connect(machineurl, { useUnifiedTopology: true }, ((err, client) => {
-    if (err) console.log(r(`Error connecting to Machine MongoDB`))
-    //db = client.db(dbName)
+const db = new Mongo(machineurl, { useNewUrlParser: true, useUnifiedTopology: true });
+db.connect(err => {
+  if (err) {
+    console.log('Error connecting to Machine')
+  } else {    
     console.log(b(`Machine MongoDB is Live`))
-    return client.db(dbName)    
-  }))
+  }
+})
 
-// // Atlas db with new collections with test data sets
-// Mongo.connect(proximityurl, { useUnifiedTopology: true }, ((err, client) => {
-//     if (err) console.log(r(`Error connecting to Proximity MongoDB`))
-//     dbProximity = client.db(proximityName)
-//     console.log(b(`Proximity MongoDB is Live`))
-    
-//   }))
 const dbProximity = new Mongo(proximityurl, { useNewUrlParser: true, useUnifiedTopology: true });
 dbProximity.connect(err => {
   if (err) {
